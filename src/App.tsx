81f3984651edc0c62d0491fc5c3d9ecc3aa5c8d4
Tomer Lev <tomerlev001@gmail.com>
2024-06-26 22:22:48 +0300

    Create a custom hook to fetch the games + added the react strict mode to the app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
 import { Button, Grid, GridItem, Show } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
+import React from "react";
 
 function App() {
   return (
     <>
-      <Grid
-        templateAreas={{
-          base: `"nav" "main"`,
-          lg: `"nav nav" "aside main"`,
-        }}
-      >
-        <GridItem area="nav">
-          <NavBar />
-        </GridItem>
-        <Show above="lg">
-          <GridItem area="aside">Aside</GridItem>
-        </Show>
-        <GridItem area="main">
-          <GameGrid />
-        </GridItem>
-      </Grid>
+      <React.StrictMode>
+        <Grid
+          templateAreas={{
+            base: `"nav" "main"`,
+            lg: `"nav nav" "aside main"`,
+          }}
+        >
+          <GridItem area="nav">
+            <NavBar />
+          </GridItem>
+          <Show above="lg">
+            <GridItem area="aside">Aside</GridItem>
+          </Show>
+          <GridItem area="main">
+            <GameGrid />
+          </GridItem>
+        </Grid>
+      </React.StrictMode>
     </>
   );
 }
